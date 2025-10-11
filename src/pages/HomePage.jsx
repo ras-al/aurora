8fcf0ef9092006a93590ac42dcc27c702f3adab6
Razin Moyi A N K <98460534+techyweird@users.noreply.github.com>
2025-10-12 00:33:35 +0530

    fixed venue maps zoom out issue

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -275,7 +275,8 @@ shaped
       <section id="location" className="location-section container common-section">
         <h2>Find Us</h2>
         <div className="map-container">
-          <iframe
+          <iframe src="https://www.google.com/maps/d/u/1/embed?mid=1pmOiMDDfQcnCG9T4Gu20fUW-1KI4lKs&ehbc=2E312F&noprof=1" width="100%" height="480"></iframe>
+          {/*<iframe
             title="Google Maps Location"
             src="https://www.google.com/maps/embed?mid=1pmOiMDDfQcnCG9T4Gu20fUW-1KI4lKs&ehbc=2E312F&noprof=1"
             width="100%"
@@ -284,7 +285,7 @@ shaped
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          ></iframe> */}
         </div>
       </section>
 
